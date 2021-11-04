fce1aadd0177c907a9d795806ecd8463ab42956f
Poul Henriksen <pohe@easj.dk>
2021-11-04 10:47:05 +0100

    Add project files.

---
+++ BigMammaUML3_41121/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigMammaUML3_41121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
